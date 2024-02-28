d3b6dde24aeb0e19c135d102b9fbde0f3b9942af
Binary-Blade <codegrunt@proton.me>
2024-02-28 23:46:15 +0100

    * refactor(init.lua): Importing module split with each folder, first version

--- init.lua
+++ init.lua
@@ -14,10 +14,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
-require('lazy').setup 'plugins'
 require 'vim-options'
 
+-- [[ Configure and install plugins ]]
+require('lazy').setup {
+  spec = {
+    { import = 'plugins' },
+    { import = 'plugins.configs' },
+    { import = 'plugins.themes' },
+    { import = 'plugins.ui' },
+  },
+}
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
