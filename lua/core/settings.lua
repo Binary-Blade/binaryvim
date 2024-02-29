022fc07f396ba0324a9ddd455bfdb3267d914511
Binary-Blade <codegrunt@proton.me>
2024-02-29 17:40:42 +0100

    * refactor(settings): Change vim command timeoutlen from 250 to 150 for improving leader key

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -15,7 +15,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 vim.opt.updatetime = 250
-vim.opt.timeoutlen = 200
+vim.opt.timeoutlen = 150
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
@@ -54,7 +54,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
