42f959fbeeb085a3fd1a4a27688fff39da66526b
Binary-Blade <codegrunt@proton.me>
2024-02-29 21:57:09 +0100

    * feat(barbar): Implant a tab logic with barbar with some keymaps inside keymaps.lua

---
+++ lua/plugins/navigation/barbar.lua
@@ -0,0 +1,28 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    animation = true,
+    insert_at_end = true,
+
+    -- TODO: Later => See more about that plugin
+    gitsigns = {
+      added = { enabled = true, icon = '+' },
+      changed = { enabled = true, icon = '~' },
+      deleted = { enabled = true, icon = '-' },
+    },
+    sidebar_filetypes = {
+      ['neo-tree'] = { event = 'BufWipeout' },
+    },
+    focus_on_close = 'left',
+
+    -- TODO: Add integrations like minivim, persistence etc
+  },
+}
