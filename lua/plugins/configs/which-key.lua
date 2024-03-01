50493fb581001b3fe28e468d8f36b9d147b2d4b3
Binary-Blade <codegrunt@proton.me>
2024-03-01 20:05:45 +0100

    * feat(which-key.lua): Add register for Obsidian, Quick keymaps and Terminal

--- lua/plugins/configs/which-key.lua
+++ lua/plugins/configs/which-key.lua
@@ -6,11 +6,13 @@ return {
 
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]oto', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]iagnostic', _ = 'which_key_ignore' },
       ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
+      ['<leader>g'] = { name = '[G]oto', _ = 'which_key_ignore' },
+      ['<leader>o'] = { name = '[O]bsidian', _ = 'which_key_ignore' },
+      ['<leader>q'] = { name = '[Q]uick', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]odo', _ = 'which_key_ignore' },
+      ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
     }
   end,
 }
