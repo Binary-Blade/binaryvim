e91f9ff8c06f6e63be1f43592831bede629a70ca
Binary-Blade <codegrunt@proton.me>
2024-03-01 19:59:54 +0100

    * refactor(autoformat.lua): Uncomment javascript prettier and prettierd

--- lua/plugins/configs/autoformat.lua
+++ lua/plugins/configs/autoformat.lua
@@ -1,5 +1,6 @@
 -- Autoformat
 return {
+  -- TODO: IMPLEMENT LATER
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = false,
@@ -9,12 +10,11 @@ return {
     },
     formatters_by_ft = {
       lua = { 'stylua' },
+      javascript = { { 'prettierd', 'prettier' } },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
-      -- You can use a sub-list to tell conform to run *until* a iiformatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
     },
   },
 }
