11a9d663406eb0a178a6c9a273cb1eed47fb689c
Binary-Blade <codegrunt@proton.me>
2024-03-29 20:58:35 +0100

    * refactor: Refactoring the tab-bar in the bottom

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,25 +1,53 @@
 return {
   'nvim-lualine/lualine.nvim',
   config = function()
+    local function mode_icon()
+      local mode_map = {
+        ['n'] = ' ', -- Icon for Normal mode
+        ['i'] = ' ', -- Icon for Insert mode
+        ['V'] = '󰅩 ', -- Icon for Visual mode
+        ['v'] = '󰘦 ', -- Icon for V-Line mode
+        ['R'] = '凜 ', -- Icon for Replace mode
+        ['c'] = ' ', -- Icon for Command mode
+        -- Add more modes as needed
+      }
+      local mode = vim.api.nvim_get_mode().mode
+      return mode_map[mode] or mode -- Fallback to mode letter if no icon defined
+    end
+
     local function lsp_status()
-      local msg = 'No Active Lsp'
+      local msg = '󰟢 LSP active' -- Default icon when no LSP is active
       local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
       local clients = vim.lsp.get_active_clients()
       if next(clients) == nil then
         return msg
       end
+
+      -- Mapping from LSP server name to icon
+      local lsp_icons = {
+        ['intelephense'] = ' ', -- Replace with your chosen PHP icon
+        ['tsserver'] = '󰛦 ', -- Replace with your chosen TypeScript icon
+        ['volar'] = ' ', -- Replace with your chosen Vue icon
+        ['lua_ls'] = '󰢱 ',
+        ['clangd'] = ' ', -- Replace with your chosen C/C++ icon
+        ['yamlls'] = '󰍛 ', -- Replace with your chosen YAML icon
+        ['dockerls'] = '󰡨 ', -- Replace with your chosen Docker icon
+      }
+
       for _, client in ipairs(clients) do
         local filetypes = client.config.filetypes
         if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-          return client.name
+          -- Return the LSP server's icon and name if defined, otherwise just the name
+          local icon = lsp_icons[client.name] or ''
+          return icon .. ' ' .. client.name
         end
       end
       return msg
     end
     require('lualine').setup {
       options = {
-        icons_enabled = true,
         theme = 'palenight',
+        icons_enabled = true,
         section_separators = { left = '', right = '' },
         component_separators = { left = '', right = '' },
         always_divide_middle = false,
@@ -31,23 +59,13 @@ return {
         },
       },
       sections = {
-        lualine_a = {
-          {
-            'fileformat',
-            symbols = {
-              unix = '', -- e712
-            },
-          },
-          'mode',
-        },
+        lualine_a = { { mode_icon, separator = { left = '', right = '' } } },
         lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic', 'coc' } } },
         lualine_c = {
           {
-            'filename',
-            path = 1,
-            shorting_target = 40,
+
             symbols = {
-              modified = '', -- Icon for modified files
+              modified = '󰔯', -- Icon for modified files
               readonly = '', -- Icon for read-only files
               unnamed = '', -- Icon for unnamed buffers
               newfile = '', -- Icon for new files
@@ -55,18 +73,16 @@ return {
           },
         },
         lualine_x = {
-          { lsp_status, icon = ' LSP:' },
+          { lsp_status, color = { fg = '#ffeead' } },
           {
             'filetype',
-            colored = true, -- Displays filetype icon in color if set to true
-            icon_only = false, -- Display only an icon for filetype
-            icon = { align = 'right' }, -- Display filetype icon on the right hand side
-            -- icon =    {'X', align='right'}
-            -- Icon string ^ in table is ignored in filetype component
+            colored = true,            -- Displays filetype icon in color if set to true
+            icon_only = false,         -- Display only an icon for filetype
+            icon = { align = 'left' }, -- Display filetype icon on the right hand side
           },
         },
         lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_z = { { 'filename', path = 1, shorting_target = 40 } },
       },
       inactive_sections = {},
       winbar = {},
