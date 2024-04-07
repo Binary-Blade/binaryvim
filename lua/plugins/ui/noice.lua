0f02b56c07aa2105f6f991acd287806239114d9a
Binary-Blade <codegrunt@proton.me>
2024-04-07 15:02:00 +0200

    * refactor: Add and transform comments

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,9 +1,6 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {
-    -- add any options here
-  },
   dependencies = {
     'MunifTanjim/nui.nvim',
     --'rcarriga/nvim-notify',
@@ -12,29 +9,14 @@ return {
     require('noice').setup {
       views = {
         cmdline_popup = {
-          position = {
-            row = 30,
-            col = '50%',
-          },
-          size = {
-            width = 70,
-            height = 'auto',
-          },
+          position = { row = 30, col = '50%' },
+          size = { width = 70, height = 'auto' },
         },
         popupmenu = {
           relative = 'editor',
-          position = {
-            row = 33,
-            col = '50%',
-          },
-          size = {
-            width = 70,
-            height = 8,
-          },
-          border = {
-            style = 'rounded',
-            padding = { 0, 1 },
-          },
+          position = { row = 33, col = '50%' },
+          size = { width = 70, height = 8 },
+          border = { style = 'rounded', padding = { 0, 1 } },
           win_options = {
             winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
           },
