9bffa295869112ab9883b9e565033aa3eefbad09
Binary-Blade <codegrunt@proton.me>
2024-04-07 15:02:23 +0200

    * refactor: Switch to true transparent

--- lua/plugins/themes/catppuccin.lua
+++ lua/plugins/themes/catppuccin.lua
@@ -6,10 +6,14 @@ return {
   config = function()
     require('catppuccin').setup {
       flavour = 'mocha',
-      --transparent_background = true,
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        notify = false,
+      },
     }
     vim.cmd.colorscheme 'catppuccin-mocha'
-    -- You can configure highlights by doing something like
-    --vim.cmd.hi 'Comment gui=none'
   end,
 }
