10e279d02549557d90e299417213f38fb0e4c657
Binary-Blade <codegrunt@proton.me>
2024-05-27 05:50:55 +0200

    * refactor: Restructure and switch max_tokens 300 to 500

--- lua/plugins/configs/chatgpt.lua
+++ lua/plugins/configs/chatgpt.lua
@@ -1,6 +1,12 @@
 return {
   'dreamsofcode-io/ChatGPT.nvim',
   event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim"
+  },
   config = function()
     require("chatgpt").setup({
       async_api_key_cmd = "pass show config/api/openai",
@@ -8,23 +14,17 @@ return {
         model = "gpt-3.5-turbo",
         frequency_penalty = 0,
         presence_penalty = 0,
-        max_tokens = 300,
+        max_tokens = 500,
         temperature = 0,
         top_p = 1,
         n = 1,
       },
       openai_edit_params = {
-        model = "gpt-3.5-turbo",
+        model = "code-davinci-002",
         temperature = 0,
-        top_p = 1,
+        max_tokens = 300,
         n = 1,
       },
     })
   end,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "folke/trouble.nvim",
-    "nvim-telescope/telescope.nvim"
-  },
 }
